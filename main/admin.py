81e5e1a1b4de566673217ab3eeac8b45324380b5
Biryukov-m <biryukov.mm@gmail.com>
2018-06-19 22:14:48 +0300

    Созданы модели для всех видов товаров, зарегистрированы в админке, проведены соответствующие миграции. В настройках указан украинский язык. Создан суперюзер.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from main.models import Genders
+from main.models import Brand
+from main.models import Fabric
+from main.models import Size
+from main.models import Color
+
+from main.models import Shirt
+from main.models import ShirtPattern
+from main.models import TShirt
+from main.models import TShirtPattern
+from main.models import Dress
+from main.models import DressPattern
+from main.models import Tunic
+from main.models import TunicPattern
+from main.models import Skirt
+from main.models import SkirtPattern
+from main.models import Bag
+from main.models import CosmeticBag
+from main.models import MobileCase
+from main.models import TableCloth
+
+
 
 # Register your models here.
+#
+# admin.site.register(Genders)
+# admin.site.register(Brand)
+# admin.site.register(Fabric)
+# admin.site.register(Size)
+# admin.site.register(Color)
+admin.site.register(Shirt)
+admin.site.register(ShirtPattern)
+admin.site.register(TShirt)
+admin.site.register(TShirtPattern)
+admin.site.register(Dress)
+admin.site.register(DressPattern)
+admin.site.register(Tunic)
+admin.site.register(TunicPattern)
+admin.site.register(Skirt)
+admin.site.register(SkirtPattern)
+admin.site.register(Bag)
+admin.site.register(CosmeticBag)
+admin.site.register(MobileCase)
+admin.site.register(TableCloth)
+
--- main/models.py
+++ main/models.py
@@ -1,48 +1,188 @@
 from django.db import models
 
-# Create your models here.
 
+# Абстрактный класс для единицы товара
 
 class Item(models.Model):
-    code = models.IntegerField(default=None, blank=True, verbose_name="Код")
-    name = models.CharField(default=None, blank=True, verbose_name='Наименование')
-    description = models.TextField(default=None, verbose_name='Описание')
-    photo = models.ImageField(default=None, blank=True, verbose_name='Основное фото')
-    price = models.IntegerField(default=None, blank=True, verbose_name='Цена')
-    available = models.BooleanField(default=False, verbose_name='Размещен в продажу')
+    code = models.PositiveIntegerField(default=None, blank=True, verbose_name="код", unique=True, editable=False)
+    name = models.CharField(default=None, blank=True, verbose_name='наименование', max_length=40)
+    description = models.TextField(default=None, verbose_name='описание', max_length=3000)
+    photo = models.ImageField(default=None, blank=True, verbose_name='основное фото')
+    price = models.IntegerField(default=None, blank=True, verbose_name='цена')
+    available = models.BooleanField(default=False, verbose_name='доступен')
 
     class Meta:
         abstract = True
 
 
 class Genders(models.Model):
-    name = models.CharField(verbose_name="Назначение")
+    name = models.CharField(verbose_name="название", max_length=30)
+
+    class Meta:
+        verbose_name = "стать"
+        verbose_name_plural = "стать"
 
 
 class Brand(models.Model):
-    name = models.CharField(verbose_name="Производитель")
-    description = models.CharField(verbose_name="Описание")
+    name = models.CharField(verbose_name="производитель", max_length=30)
+    description = models.CharField(verbose_name="описание", max_length=3000)
+
+    class Meta:
+        verbose_name = "виробник"
+        verbose_name_plural = "виробники"
 
 
 class Fabric(models.Model):
-    name = models.CharField(verbose_name="Ткань")
+    name = models.CharField(verbose_name="название", max_length=30)
+    description = models.CharField(verbose_name="описание", max_length=3000)
+
+    class Meta:
+        verbose_name = "тканина"
+        verbose_name_plural = "тканини"
 
 
 class Color(models.Model):
-    name = models.CharField(verbose_name="Цвет")
+    name = models.CharField(verbose_name="цвет", max_length=30)
+
+    class Meta:
+        verbose_name = "колір"
+        verbose_name_plural = "кольори"
 
 
 class Size(models.Model):
-    name = models.CharField(verbose_name="Размер")
+    name = models.CharField(verbose_name="размер", max_length=20)
 
+    class Meta:
+        verbose_name = "розмір"
+        verbose_name_plural = "розміри"
+
+
+# Абстрактный класс для элементов одежды
 
 class Clothes(Item):
-    gender = models.ForeignKey(Genders, on_delete=models.PROTECT)
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
-    fabric = models.ForeignKey(Fabric, on_delete=models.PROTECT)
-    color = models.ForeignKey(Color, on_delete=models.PROTECT)
-    size = models.ForeignKey(Size, on_delete=models.PROTECT)
+    gender = models.ForeignKey(Genders, on_delete=models.PROTECT, verbose_name="стать")
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name="виробник")
+    fabric = models.ForeignKey(Fabric, on_delete=models.PROTECT, verbose_name="тканина")
+    color = models.ForeignKey(Color, on_delete=models.PROTECT, verbose_name="колір")
+    size = models.ForeignKey(Size, on_delete=models.PROTECT, verbose_name="розмір")
 
     class Meta:
         abstract = True
 
+
+# Абстрактный класс для заготовок
+
+class Pattern(models.Model):
+    partial = models.BooleanField(default=False, verbose_name="з відрізами")
+    sleeve_length = models.IntegerField(default=None, blank=True, verbose_name="довжина рукава")
+    sleeve_width = models.IntegerField(default=None, blank=True, verbose_name="ширина рукава")
+    body_length = models.IntegerField(default=None, blank=True, verbose_name="довжина основи")
+    body_width = models.IntegerField(default=None, blank=True, verbose_name="ширина основи")
+
+    class Meta:
+        abstract = True
+        verbose_name = "заготовка"
+        verbose_name_plural = "заготовки"
+
+
+# Товары
+
+class Shirt(Clothes):
+
+    class Meta:
+        verbose_name = "сорочка"
+        verbose_name_plural = "сорочки"
+
+
+class ShirtPattern(Clothes, Pattern):
+
+    class Meta:
+        verbose_name = "заготовка сорочки"
+        verbose_name_plural = "заготовки сорочок"
+
+
+class TShirt(Clothes):
+
+    class Meta:
+        verbose_name = "футболка"
+        verbose_name_plural = "футболки"
+
+
+class TShirtPattern(Clothes, Pattern):
+
+    class Meta:
+        verbose_name = "заготовка футболки"
+        verbose_name_plural = "заготовки футболок"
+
+
+class Dress(Clothes):
+
+    class Meta:
+        verbose_name = "сукня"
+        verbose_name_plural = "сукні"
+
+
+class DressPattern(Clothes, Pattern):
+
+    class Meta:
+        verbose_name = "заготовка сукні"
+        verbose_name_plural = "заготовки суконь"
+
+
+class Tunic(Clothes):
+
+    class Meta:
+        verbose_name = "туніка"
+        verbose_name_plural = "туніки"
+
+
+class TunicPattern(Clothes, Pattern):
+
+    class Meta:
+        verbose_name = "заготовка туніки"
+        verbose_name_plural = "заготовки тунік"
+
+
+class Skirt(Clothes):
+
+    class Meta:
+        verbose_name = "спідниця"
+        verbose_name_plural = "спідниці"
+
+
+class SkirtPattern(Clothes, Pattern):
+
+    class Meta:
+        verbose_name = "заготовка спідниці"
+        verbose_name_plural = "заготовки спідниць"
+
+
+# Аксессуари
+
+
+class Bag(Item):
+
+    class Meta:
+        verbose_name = "сумка"
+        verbose_name_plural = "сумки"
+
+
+class CosmeticBag(Item):
+
+    class Meta:
+        verbose_name = "косметичка"
+        verbose_name_plural = "косметички"
+
+
+class MobileCase(Item):
+
+    class Meta:
+        verbose_name = "чохол для мобільного телефону"
+        verbose_name_plural = "чохли для мобільних телефонів"
+
+
+class TableCloth(Item):
+
+    class Meta:
+        verbose_name = "скатертина"
+        verbose_name_plural = "скатертини"
