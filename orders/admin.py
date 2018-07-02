e80f390c4cb336685a8a7f263a440f618373b4be
Biryukov <biryukov.mm@gmail.com>
2018-07-02 12:03:11 +0300

    Создано приложение "Orders" для управления заказами. В модели ProductInOrder задействованы Content-type и GenericRelations, с которыми еще предстоит разобраться. Пока, в админке Orders, в модели ProductInOrder отображается примерно то, что нужно. В поле ContentType - вываливается список всех моделей, к одной из которых нужно привязать продукт, и указать его id.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Order
+from .models import ProductInOrder
+
+admin.site.register(Order)
+admin.site.register(ProductInOrder)
 
 # Register your models here.
