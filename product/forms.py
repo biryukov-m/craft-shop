d43a58465efb129ead166b0938460c34acf60fe9
Max <biryukov.mm@gmail.com>
2018-09-26 17:29:07 +0300

    фильтр через метакласс протеитирован

--- product/forms.py
+++ product/forms.py
@@ -3,16 +3,21 @@ from product.models import Item
 
 
 class ProductFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='iexact')
+    # name = django_filters.CharFilter(lookup_expr='iexact')
+    #
+    # price = django_filters.NumberFilter()
+    # price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
+    # price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    #
+    # # brand__name = django_filters.MultipleChoiceFilter(field_name='brand__name', lookup_expr='iexact')
+    # # gender__name = django_filters.MultipleChoiceFilter(field_name='gender__name', lookup_expr='iexact')
+    # # fabric__name = django_filters.MultipleChoiceFilter(field_name='fabric__name', lookup_expr='iexact')
+    # # color__name = django_filters.MultipleChoiceFilter(field_name='color__name', lookup_expr='iexact')
+    # brand__name = django_filters.ChoiceFilter(field_name='brand__name', lookup_expr='iexact')
+    # gender = django_filters.ChoiceFilter(field_name='gender__name', lookup_expr='iexact')
+    # fabric = django_filters.ChoiceFilter(field_name='fabric__name', lookup_expr='iexact')
+    # color = django_filters.ChoiceFilter(field_name='color__name', lookup_expr='iexact')
 
-    price = django_filters.NumberFilter()
-    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
-    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
-
-    brand__name = django_filters.MultipleChoiceFilter(field_name='brand__name')
-    gender__name = django_filters.MultipleChoiceFilter(field_name='gender__name')
-    fabric__name = django_filters.MultipleChoiceFilter(field_name='fabric__name')
-    color__name = django_filters.MultipleChoiceFilter(field_name='color__name')
-
-    # class Meta:
-        # model = Item
+    class Meta:
+        model = Item
+        fields = ['price', 'brand', 'gender', 'fabric', 'color', 'size', 'item_type']
