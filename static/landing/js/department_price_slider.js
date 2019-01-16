1d821e4a744b91c2019ea01296f7387d2aea33a9
Max <biryukov.mm@gmail.com>
2019-01-16 10:48:24 +0200

    Добавлены все статик файлы, потерялся скрипт js для страницы с одним товаром

---
+++ static/landing/js/department_price_slider.js
@@ -0,0 +1,14 @@
+
+$( function() {
+    $( "#slider-price-range" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+            $( "#price-amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+    });
+    $( "#price-amount" ).val( "$" + $( "#slider-price-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-price-range" ).slider( "values", 1 ) );
+} );
