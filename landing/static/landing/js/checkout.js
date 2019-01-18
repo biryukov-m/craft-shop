6c73945b1d08607b0894fd75c14e24e0bb8abc02
biryukov <biryukov>
2019-01-18 06:30:17 +0200

    Продвижение по чекауту. Дописана форма для отправки заказа.

---
+++ landing/static/landing/js/checkout.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('label#add-comment').on('click', function (e) {
+        // $('#id_customer_comment').css('display', 'block');
+        $('#id_customer_comment').toggle();
+    })
+});
